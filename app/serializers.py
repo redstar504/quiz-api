d82d586a0ce0ac559292a1ecc74eaad75d42e867
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-20 19:42:44 -0800

    use a serializer method field for questions

--- app/serializers.py
+++ app/serializers.py
@@ -25,11 +25,15 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 
 class TopicDetailSerializer(serializers.ModelSerializer):
-    questions = QuestionSerializer(many=True, source='question_set')
+    questions = serializers.SerializerMethodField()
 
     class Meta:
         model = Topic
         fields = ['id', 'title', 'icon', 'color', 'questions']
+
+    def get_questions(self, obj):
+        questions = obj.question_set.all().order_by('id')
+        return QuestionSerializer(questions, many=True).data
 
 
 class TopicListSerializer(serializers.ModelSerializer):
