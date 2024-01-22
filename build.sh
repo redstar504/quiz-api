3f15bbdc36ea6e86c6ad960f7f3aa4e169d98324
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-21 19:23:39 -0800

    continue configuring celery

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-nohup celery -A quiz worker --concurrency=4 --loglevel=debug --logfile=celery.log > celery.out 2>&1 &
--- quiz/celery.py
+++ quiz/celery.py
@@ -11,16 +11,3 @@ app.autodiscover_tasks()
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
-
-# RENDER_REDIS_BACKEND_URL = os.environ.get('CELERY_BROKER_URL')
-#
-# if RENDER_REDIS_BACKEND_URL:
-#     CELERY_BROKER_URL = RENDER_REDIS_BACKEND_URL
-#     CELERY_RESULT_BACKEND = RENDER_REDIS_BACKEND_URL
-# else:
-#     CELERY_BROKER_URL = 'redis://localhost'
-#     CELERY_RESULT_BACKEND = 'redis://localhost'
-#
-# celery_app = Celery('quiz')
-#
-# celery_app.conf.result_backend = CELERY_RESULT_BACKEND
