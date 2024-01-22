15172d3061ec2a02a85fe3d6dd46a00dcc9c462e
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-22 14:33:01 -0800

    add a production celery command

--- app/management/commands/celery.py
+++ app/management/commands/celery.py
@@ -3,8 +3,24 @@ import subprocess
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--production",
+            action="store_true",
+            dest="prod",
+            help="Start celery in production mode"
+        )
+
     def handle(self, *args, **options):
-        cmd = 'watchmedo auto-restart --directory=./app --pattern=*.py --recursive -- celery -A quiz worker --loglevel=info'
-        print('Starting celery worker with autoreload...')
-        subprocess.call(shlex.split(cmd))
+        if options['prod']:
+            print("start celery prod")
+            cmd = 'celery -A quiz worker --loglevel=info --concurrency=4'
+            subprocess.Popen(shlex.split(cmd))
+        else:
+            print('Starting celery worker with autoreload...')
+            cmd = 'watchmedo auto-restart --directory=./app --pattern=*.py --recursive -- celery -A quiz worker --loglevel=info'
+            subprocess.call(shlex.split(cmd))
+
