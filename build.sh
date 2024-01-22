57c99cdfe40bfcb34a9c67c4082f9cfc737b335e
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-21 18:20:12 -0800

    update celery command in build script

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-celery -A quiz worker --concurrency=4 --loglevel=debug --logfile=celery.log --detach
+nohup celery -A quiz worker --concurrency=4 --loglevel=debug --logfile=celery.log > celery.out 2>&1 &
