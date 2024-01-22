c16fa2aace7106e50975791b756395f66c2a1999
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-22 15:06:44 -0800

    cels

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-gunicorn quiz.wsgi:application
+gunicorn quiz.wsgi:application &
+
+sleep 10
+
+echo "start celery"
+
+celery -qA quiz worker --loglevel=fatal &
 
 #nohup python manage.py celery --production &
