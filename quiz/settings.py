c65e02a70922cc2c4f6208119d15472e733795af
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-21 16:22:40 -0800

    add celery config

--- quiz/settings.py
+++ quiz/settings.py
@@ -135,3 +135,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://red-cmmp2mf109ks739alj5g:6379"
+CELERY_RESULT_BACKEND = "redis://red-cmmp2mf109ks739alj5g:6379"
