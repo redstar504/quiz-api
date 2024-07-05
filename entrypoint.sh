bfcf97db9c4a42a6231654e611007b8e5bd8bfd9
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-07-04 19:34:28 -0700

    update entrypoint for image role

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ set -o errexit
 
 export DJANGO_SETTINGS_MODULE=quiz.settings
 python manage.py migrate
-exec gunicorn -b 0.0.0.0 quiz.wsgi
+
+if [ "$ROLE" == "worker" ]; then
+  echo "Starting worker..."
+  celery -A quiz worker -l INFO
+else
+  echo "Starting server..."
+  exec gunicorn -b 0.0.0.0 quiz.wsgi
+fi
