a20d5633aafe41fbf94e74e055686fa493e799e4
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-07-27 23:32:02 -0700

    add worker restart to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 docker build --no-cache -t redstar504/quiz-backend:latest .
 docker push redstar504/quiz-backend:latest
 kubectl rollout restart deployment quiz-api
+kubectl rollout restart deployment quiz-worker
